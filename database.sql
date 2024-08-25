b489d952b563bb4bc6f6bd254ce34160b5945bde
Shamil Kh <shamilist90@gmail.com>
2024-08-25 11:52:03 +0300

    step2

--- database.sql
+++ database.sql
@@ -52,3 +52,29 @@ CREATE TABLE ProgramModule (
   FOREIGN KEY (program_id) REFERENCES Programs(id),
   FOREIGN KEY (module_id) REFERENCES Modules(id)
 );
+
+CREATE TABLE Users (
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  FOREIGN KEY (role_id) REFERENCES UserRole (id),
+  full_name VARCHAR(255),
+  email VARCHAR(255),
+  password_hash VARCHAR(255),
+  FOREIGN KEY (teaching_group_id) REFERENCES TeachingGroups (id),
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP
+);
+
+CREATE TABLE TeachingGroups (
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  slug VARCHAR(255),
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP
+);
+
+CREATE TABLE UserRole (
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255)
+);
+
+INSERT INTO UserRole VALUES ('сутдент', 'учитель', 'админ');
+
